4541653838eec8c4ea39845a69115a1727d2309f
tdyleuth <tony.dyleuth@gmail.com>
2020-08-19 19:18:38 -0700

    Updates

---
+++ precache-manifest.0ee5ed0fca05abbb97c09025cc108a02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5829415cae797da744a18517fb2aa83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27587e76fde498ab03d5",
+    "url": "/static/css/main.1fa83fe2.chunk.css"
+  },
+  {
+    "revision": "1e885d8059f374fed015",
+    "url": "/static/js/2.4a17969d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4a17969d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27587e76fde498ab03d5",
+    "url": "/static/js/main.d38eba23.chunk.js"
+  },
+  {
+    "revision": "99a3d2817b1f43b2b164",
+    "url": "/static/js/runtime-main.99ff291e.js"
+  }
+]);
