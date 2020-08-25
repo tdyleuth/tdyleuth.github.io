dbff0ee3f081cb5e2ae1387807f668ff0615d0ed
tdyleuth <tony.dyleuth@gmail.com>
2020-08-24 17:39:50 -0700

    update header css

---
+++ build/precache-manifest.0a3f342dc83c7fd2c9cd1825fdcb42d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6436576c1e4daa95a97d5645e0173041",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d82bc664ac0d74ff9340",
+    "url": "/static/css/main.1fa83fe2.chunk.css"
+  },
+  {
+    "revision": "1e885d8059f374fed015",
+    "url": "/static/js/2.4a17969d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4a17969d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d82bc664ac0d74ff9340",
+    "url": "/static/js/main.f25ed546.chunk.js"
+  },
+  {
+    "revision": "99a3d2817b1f43b2b164",
+    "url": "/static/js/runtime-main.99ff291e.js"
+  }
+]);
